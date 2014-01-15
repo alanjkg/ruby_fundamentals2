4dd238dd06c4c5d028f67ae2d8ce344871234284
alanjkg <alanjkg@gmail.com>
2014-01-15 17:01:21 -0500

    finished exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,17 +1,29 @@
+#part 1
+
 def list (groceries)
 groceries.each {|item| puts "*"+item}
 end
 
 groceries = ["carrots", "toilet paper", "apples", "salmon"]
+#part 2
 groceries.push ("rice")
 list (groceries)
+
+#part 3
 puts groceries.length
 
+#part 4
 if groceries.include? ("banana")
 	puts "You need to pick up bananas"
 else
 	puts "You don't need to pick up bananas"
 end
 
+#part 5
 puts groceries[1]
-puts groceries.sort
+
+#part 6
+list (groceries).sort
+#part 7
+groceries.delete_at(3)
+list (groceries)
