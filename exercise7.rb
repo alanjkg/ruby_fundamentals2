f018b29182ac8d9a6c48604e51ea39e8609751a9
alanjkg <alanjkg@gmail.com>
2014-01-15 18:43:25 -0500

    finished loop on bonus

--- exercise7.rb
+++ exercise7.rb
@@ -1,6 +1,39 @@
+#part 1
+def list (students)
+students.each {|key,value| puts "#{key}:#{value} students"}
+end
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
-students.each {|x,y| puts "#{x}:#{y} students"}
+list (students)
+#part 2
+
+students [:cohort4] = 43
+list (students)
+
+#part 3
+students.each do |key, value|
+  puts "The key is #{key} and the value is #{value}"
+end
+
+#part 4
+
+students.each do |key, value|
+  students[key] = (value *1.05).to_int
+end
+list (students)
+
+#part 5
+
+students.delete (:cohort2)
+list (students)
+
+#Bonus
+total_number_of_students = 0
+students.each do |key, value|
+  total_number_of_students = total_number_of_students + value 
+end
+puts "the total number of students is #{total_number_of_students}"
